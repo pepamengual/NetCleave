64ebb06ab6050cae9f8a4d68879a1c069259d062
Pep Amengual <pep@MacBook-Pro-de-Pep.local>
2020-03-18 16:57:53 +0100

    Main cleaner version. Reclycling generate data for training and scoring set functions

--- PROcleave.py
+++ PROcleave.py
@@ -20,18 +20,13 @@ def parse_args():
     args = parser.parse_args()
     return args.generate, args.train, args.predict_fasta, args.score_set
 
-def generating_training_data(iedb_path, uniprot_path, training_data_path):
-    print("Generating training data...")
-    mhc_class = "I"
-    iedb_data = peptide_extractor.extract_peptide_data(iedb_path, mhc_class) #Reading IEDB data, consider add argument about MHC class
-    uniprot_data = uniprot_extractor.extract_uniprot_data(uniprot_path) #Extracting uniprot data
-    proteasome_dictionary = peptide_uniprot_locator.locate_peptides(iedb_data, uniprot_data) #Finding neighbours
-    training_data_generator.prepare_cleavage_data(proteasome_dictionary, training_data_path)
+def generating_data(iedb_path, uniprot_path, conditions):
+    iedb_data = peptide_extractor.extract_peptide_data(iedb_path, conditions)
+    uniprot_data = uniprot_extractor.extract_uniprot_data(uniprot_path)
+    proteasome_dictionary = peptide_uniprot_locator.locate_peptides(iedb_data, uniprot_data)
+    return proteasome_dictionary
 
 def main(generate=False, train=False, predict_fasta=False, score_set=False):
-    iedb_path = "../../data/raw/iedb/mhc_ligand_full.csv"
-    uniprot_path = "../../data/raw/uniprot/uniprot_sprot.fasta"
-    
     iedb_path = "../PROcleave_data/mhc_ligand_full.csv" # download and unzip from http://www.iedb.org/database_export_v3.php
     uniprot_path = "../PROcleave_data/uniprot_sprot.fasta" # download and decompress from https://www.uniprot.org/downloads REVIEWED fasta
     training_data_path = "data/training_data/proteasome"
@@ -39,22 +34,28 @@ def main(generate=False, train=False, predict_fasta=False, score_set=False):
 
     if not any([generate, train, predict_fasta, score_set]):
         print("Please, provide an argument. See python3 PROcleave.py -h for more information")
+    
     if generate:
-        generating_training_data(iedb_path, uniprot_path, training_data_path)
+        conditions = {"Description": None, "Parent Protein IRI": None, 
+                      "Method/Technique": ("contains", "mass spectrometry"), "MHC allele class": ("match", "I")}
+        proteasome_dictionary = generating_data(iedb_path, uniprot_path, conditions)
+        training_data_generator.prepare_cleavage_data(proteasome_dictionary, training_data_path)
+
     if train:
         training_engine.create_models(training_data_path, models_export_path)
+    
     if predict_fasta:
         sequence = "LVVSFVVGGLAPKLEDIDLE"
         peptide_lenght_list = [8, 9, 10]
         peptide_list = predictor_fasta.proteasome_prediction(sequence, models_export_path, peptide_lenght_list)
+    
     if score_set:
-        export_path = "data/score_set/class_{}/class_{}_data.csv".format(mhc_class, mhc_class)
-        conditions_dictionary = {"Description": None, "Parent Protein IRI": None, "Method/Technique": ("contains", "mass spectrometry"), "MHC allele class": ("match", "I")}
-        iedb_data = peptide_extractor.extract_peptide_data(iedb_path, conditions_dictionary)
-        uniprot_data = uniprot_extractor.extract_uniprot_data(uniprot_path)
-        proteasome_dictionary = peptide_uniprot_locator.locate_peptides(iedb_data, uniprot_data)
-        scoring_data_generator.generating_scoring_data(proteasome_dictionary, export_path)
-        predictor_set.proteasome_prediction(export_path, models_export_path)
+        conditions = {"Description": None, "Parent Protein IRI": None, 
+                      "Method/Technique": ("contains", "mass spectrometry"), "MHC allele class": ("match", "II")}
+        export_set_path = "data/score_set/class_{0}/class_{0}_data.csv".format(conditions["MHC allele class"][1])
+        proteasome_dictionary = generating_data(iedb_path, uniprot_path, conditions)
+        scoring_data_generator.generating_scoring_data(proteasome_dictionary, export_set_path)
+        predictor_set.proteasome_prediction(export_set_path, models_export_path)
 
 if __name__ == "__main__":
     generate, train, predict_fasta, score_set = parse_args()
