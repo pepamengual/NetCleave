4c442ce1e9c681ebd07873a413d6d96d87ed897a
Pep Amengual Rigo <pep@MacBook-Pro-de-Pep.local>
2021-01-23 10:32:06 +0100

    Removing deprecated info

--- deprecated/predictor/ml_main/ml_utilities/read_table.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def read_training_table(table_path):
-    print("Reading training table {}...".format(table_path))
-    training_table = pd.read_table(table_path)
-    print("Training table {} read...".format(table_path))
-    return training_table
-
