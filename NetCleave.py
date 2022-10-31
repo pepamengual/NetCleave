6ecbe6a7ffb329869f47755c35d15b3a87208894
pepamengual <pepamengualrigo@gmail.com>
2022-10-31 10:05:06 +0100

    Cleaning up code

--- NetCleave.py
+++ NetCleave.py
@@ -1,4 +1,7 @@
 import argparse
+import gzip
+import shutil
+import os
 from predictor.database_functions import peptide_extractor, uniprot_extractor, uniparc_extractor
 from predictor.core import all_peptide_uniprot_locator, all_training_data_generator
 from predictor.ml_main import run_NN
@@ -20,6 +23,14 @@ def parse_args():
     return args.generate, args.train, args.score_csv
 
 
+def decompress_databases(database_list):
+    for database_file in database_list:
+        if not os.path.exists(database_file):
+            database_file_compressed = f"{database_file}.gz"
+            with gzip.open(database_file_compressed, "r") as f_in, open(database_file, "wb") as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+
 def generating_data(iedb_path, uniprot_path, uniparc_path_headers, uniparc_path_sequence, conditions):
     iedb_data = peptide_extractor.extract_peptide_data(iedb_path, conditions)
     uniprot_data = uniprot_extractor.extract_uniprot_data(uniprot_path)
@@ -33,6 +44,14 @@ def main(generate=False, train=False, score_csv=False):
     mhc_class, technique, mhc_family = "I", "mass spectrometry", "HLA-A"
     technique_name = technique.replace(" ", "-")
     mhc_family_name = mhc_family.replace("*", "").replace(":", "")
+
+    iedb_path = "data/databases/iedb/mhc_ligand_full.csv"  # download and unzip from http://www.iedb.org/database_export_v3.php
+    uniprot_path = "data/databases/uniprot/uniprot_sprot.fasta"  # download and decompress from https://www.uniprot.org/downloads REVIEWED fasta
+    uniparc_path_headers = "data/databases/uniparc/uniparc-yourlist_M20200416A94466D2655679D1FD8953E075198DA854EB3ES.tab"
+    uniparc_path_sequence = "data/databases/uniparc/uniparc-yourlist_M20200416A94466D2655679D1FD8953E075198DA854EB3ES.fasta"
+    database_list = [iedb_path, uniprot_path, uniparc_path_headers, uniparc_path_sequence]
+    decompress_databases(database_list)
+
     training_data_path = f"data/training_data/{mhc_class}_{technique_name}_{mhc_family_name}"
     models_export_path = f"data/models/{mhc_class}_{technique_name}_{mhc_family_name}"
 
@@ -40,11 +59,6 @@ def main(generate=False, train=False, score_csv=False):
         print("Please, provide an argument. See python3 NetCleave.py -h for more information")
     
     if generate:
-        iedb_path = "data/mhc_ligand_full.csv" # download and unzip from http://www.iedb.org/database_export_v3.php
-        uniprot_path = "data/uniprot/uniprot_sprot.fasta" # download and decompress from https://www.uniprot.org/downloads REVIEWED fasta
-        uniparc_path_headers = "data/uniparc/uniparc-yourlist_M20200416A94466D2655679D1FD8953E075198DA854EB3ES.tab"
-        uniparc_path_sequence = "data/uniparc/uniparc-yourlist_M20200416A94466D2655679D1FD8953E075198DA854EB3ES.fasta"
-
         conditions = {"Description": None, "Parent Protein IRI": None, 
                       "Method/Technique": ("contains", technique),
                       "MHC allele class": ("match", mhc_class),
